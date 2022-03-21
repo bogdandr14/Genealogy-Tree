2780f8d2e84bd92a589f4d424879a607bc4e699f
Bogdan Draghici <bogdandraghici99@gmail.com>
2022-03-21 23:24:47 +0200

    added functionality for redirect to details/edit

--- gene-tree/src/app/modules/tree/tree.page.ts
+++ gene-tree/src/app/modules/tree/tree.page.ts
@@ -1,7 +1,8 @@
 import { PersonService } from './../person/services/person.service';
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import FamilyTree from '@balkangraph/familytree.js';
 import { DataService } from '../core/services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tree',
@@ -10,75 +11,85 @@ import { DataService } from '../core/services/data.service';
 })
 export class TreePage implements OnInit {
   public familyTree: FamilyTree;
-  constructor(private dataService: DataService,
-    private personService: PersonService) { }
+  private personDetailHandler = (nodeId) => {
+    var nodeData = this.familyTree.get(nodeId);
+    var person = nodeData['personId'];
+    this.router.navigate(['/person/details', 1]);
+  };
 
+  private personEditHandler = (nodeId) => {
+    var nodeData = this.familyTree.get(nodeId);
+    var person = nodeData['personId'];
+    this.router.navigate(['/person/edit', 1]);
+  };
+
+  constructor(
+    private router: Router,
+    private dataService: DataService,
+    private personService: PersonService
+  ) {}
+
+  //href="tel://{val}
   ngOnInit() {
+    this.initFamilyTreeTemplates();
     this.initFamilyTree();
     this.setConversions();
-    this.loatFamilyTree();
+    this.loadFamilyTree();
     this.setThemeSubscriber();
-
   }
-  initFamilyTree() {
-    const userIcon = '<path style="fill: white;" d="M332.64 64.58C313.18 43.57 286 32 256 32c-30.16 0-57.43 11.5-76.8 32.38-19.58 21.11-29.12 49.8-26.88 80.78C156.76 206.28 203.27 256 256 256s99.16-49.71 103.67-110.82c2.27-30.7-7.33-59.33-27.03-80.6zM432 480H80a31 31 0 01-24.2-11.13c-6.5-7.77-9.12-18.38-7.18-29.11C57.06 392.94 83.4 353.61 124.8 326c36.78-24.51 83.37-38 131.2-38s94.42 13.5 131.2 38c41.4 27.6 67.74 66.93 76.18 113.75 1.94 10.73-.68 21.34-7.18 29.11A31 31 0 01432 480z"/>';
-    FamilyTree.templates.hugo_male['field_2'] ='<g transform="translate(10,10)"><g transform="matrix(0.046875 0 0 0.046875 4.000001 -0)"><a target="_blank" xlink:href="tel://{val}">' + userIcon + '</a></g></g>';
-    FamilyTree.templates.hugo_female['field_2'] ='<a target="_blank" xlink:href="tel://{val}"><rect width="64" height="64" fill="#ffffff" opacity=0></rect><g transform="translate(10,10)"><g transform="matrix(0.046875 0 0 0.046875 4.000001 -0)">' + userIcon + '</g></g></rect></a>';
 
+  initFamilyTreeTemplates() {
+    const userProfileLink = 'http://localhost:8100/user/profile/';
+    const userIcon =
+      '<path style="fill: white;" d="M332.64 64.58C313.18 43.57 286 32 256 32c-30.16 0-57.43 11.5-76.8 32.38-19.58 21.11-29.12 49.8-26.88 80.78C156.76 206.28 203.27 256 256 256s99.16-49.71 103.67-110.82c2.27-30.7-7.33-59.33-27.03-80.6zM432 480H80a31 31 0 01-24.2-11.13c-6.5-7.77-9.12-18.38-7.18-29.11C57.06 392.94 83.4 353.61 124.8 326c36.78-24.51 83.37-38 131.2-38s94.42 13.5 131.2 38c41.4 27.6 67.74 66.93 76.18 113.75 1.94 10.73-.68 21.34-7.18 29.11A31 31 0 01432 480z"/>';
+    const userButtonTemplate =
+      '<g transform="translate(10,10)"><a xlink:href="' +
+      userProfileLink +
+      '{val}"><rect width="32" height="24" fill="#ffffff" opacity=0></rect><g transform="matrix(0.046875 0 0 0.046875 4.000001 -0)">' +
+      userIcon +
+      '</g></a></g>';
+    FamilyTree.templates.hugo_male['field_2'] = userButtonTemplate;
+    FamilyTree.templates.hugo_female['field_2'] = userButtonTemplate;
+
+    const personImgTemplate =
+      '<clipPath id="ulaImg">' +
+      '<circle cx="125" cy="20" r="40"></circle>' +
+      '</clipPath>' +
+      '<image preserveAspectRatio="xMidYMid slice" stroke="white" clip-path="url(#ulaImg)" xlink:href="{val}" x="85" y="-20" width="80" height="80">' +
+      '</image>';
+    FamilyTree.templates.hugo_male['img_0'] = personImgTemplate;
+    FamilyTree.templates.hugo_female['img_0'] = personImgTemplate;
+  }
+
+  initFamilyTree() {
     this.familyTree = new FamilyTree(document.getElementById('tree'), {
+      nodeMouseClick: FamilyTree.action.none,
       mouseScrool: FamilyTree.action.zoom,
       mode: 'dark',
       template: 'hugo',
       miniMap: false,
       roots: [3],
       nodeMenu: {
-        edit: { text: 'Edit' },
-        details: { text: 'Details' },
+        edit: {
+          text: 'Edit',
+          icon: FamilyTree.icon.edit(18, 18, '#039BE5'),
+          onClick: this.personEditHandler,
+        },
+        details: {
+          text: 'Details',
+          icon: FamilyTree.icon.details(18, 18, '#039BE5'),
+          onClick: this.personDetailHandler,
+        },
       },
       nodeBinding: {
         field_0: 'name',
         field_1: 'born',
-        field_2: 'user',
+        field_2: 'userId',
         img_0: 'photo',
       },
-      editForm: {
-        titleBinding: 'name',
-        photoBinding: 'photo',
-        addMoreBtn: 'Add element',
-        addMore: 'Add more elements',
-        addMoreFieldName: 'Element name',
-        generateElementsFromFields: false,
-        // elements: [
-        //   { type: 'textbox', label: 'Full Name', binding: 'name' },
-        //   { type: 'textbox', label: 'Email Address', binding: 'email' },
-        //   [
-        //     { type: 'textbox', label: 'Phone', binding: 'phone' },
-        //     { type: 'date', label: 'Date Of Birth', binding: 'born' },
-        //   ],
-        //   [
-        //     {
-        //       type: 'select',
-        //       options: [
-        //         { value: 'bg', text: 'Bulgaria' },
-        //         { value: 'ru', text: 'Russia' },
-        //         { value: 'gr', text: 'Greece' },
-        //       ],
-        //       label: 'Country',
-        //       binding: 'country',
-        //     },
-        //     { type: 'textbox', label: 'City', binding: 'city' },
-        //   ],
-        //   {
-        //     type: 'textbox',
-        //     label: 'Photo Url',
-        //     binding: 'photo',
-        //     btn: 'Upload',
-        //   },
-        // ],
-      },
     });
-
   }
+
   setConversions() {
     this.familyTree.on('field', (sender, args: any) => {
       if (args.name == 'born') {
@@ -88,7 +99,7 @@ export class TreePage implements OnInit {
     });
   }
 
-  loatFamilyTree() {
+  loadFamilyTree() {
     this.familyTree.load([
       {
         id: 1,
@@ -97,7 +108,7 @@ export class TreePage implements OnInit {
         photo: 'https://cdn.balkan.app/shared/m60/2.jpg',
         name: 'Zeph Daniels',
         born: '1954-09-29',
-        user: null
+        userId: null,
       },
       {
         id: 2,
@@ -106,7 +117,7 @@ export class TreePage implements OnInit {
         photo: 'https://cdn.balkan.app/shared/m60/1.jpg',
         name: 'Rowan Annable',
         born: '1952-10-10',
-        user: 'adad'
+        userId: 'E6CC4808-683F-489B-9C1C-97E156F2CB27',
       },
       {
         id: 3,
@@ -119,14 +130,15 @@ export class TreePage implements OnInit {
         phone: '+44 845 5752 547',
         city: 'Moscow',
         country: 'ru',
-        user: 'adsad'
+        userId: 'E6CC4808-683F-489B-9C1C-97E156F2CB27',
       },
       {
         id: 4,
         pids: [5],
+        gender: 'other',
         photo: 'https://cdn.balkan.app/shared/m60/3.jpg',
         name: 'Rowan Annable',
-        user: 'asdadad'
+        userId: 'E6CC4808-683F-489B-9C1C-97E156F2CB27',
       },
       {
         id: 5,
@@ -134,7 +146,7 @@ export class TreePage implements OnInit {
         gender: 'female',
         photo: 'https://cdn.balkan.app/shared/w60/3.jpg',
         name: 'Lois Sowle',
-        user: 'asadsd'
+        userId: 'E6CC4808-683F-489B-9C1C-97E156F2CB27',
       },
       {
         id: 6,
@@ -145,7 +157,7 @@ export class TreePage implements OnInit {
         photo: 'https://cdn.balkan.app/shared/w30/1.jpg',
         name: 'Tyler Heath',
         born: '1975-11-12',
-        user: 'asdad'
+        userId: 'E6CC4808-683F-489B-9C1C-97E156F2CB27',
       },
       {
         id: 7,
@@ -156,7 +168,7 @@ export class TreePage implements OnInit {
         photo: 'https://cdn.balkan.app/shared/m30/3.jpg',
         name: 'Samson Stokes',
         born: '1986-10-01',
-        user: 'asdada'
+        userId: 'E6CC4808-683F-489B-9C1C-97E156F2CB27',
       },
       {
         id: 8,
@@ -166,10 +178,11 @@ export class TreePage implements OnInit {
         photo: 'https://cdn.balkan.app/shared/w10/3.jpg',
         name: 'Celeste Castillo',
         born: '2021-02-01',
-        user: 'adasdad'
+        userId: 'E6CC4808-683F-489B-9C1C-97E156F2CB27',
       },
     ]);
   }
+
   private setThemeSubscriber() {
     this.dataService.darkTheme$.subscribe((dark) => {
       const themeString = dark ? 'dark' : 'light';
